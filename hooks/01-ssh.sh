897676d7395e909a3f3ae15484def512d3d355e7
Phil Cryer <phil@philcryer.com>
2012-06-01 01:56:00 +0000

    cleaned up hookfiles, more comments telling user what's up

--- hooks/01-ssh.sh
+++ hooks/01-ssh.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-echo "HOOK: ssh server"
-# install
-echo "I: installing openssh-server"
+echo "I: Installing openssh-server"
 apt-get install --yes --force-yes -y openssh-server
 # disable root login
-echo "I: disabling root login in sshd_config"
+echo "I: Disabling root login in sshd_config"
 sed -i "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
-# don't start ssh on boot if you don't need it
+# don't start ssh on boot, we don't need it
+echo "I: Disabling openssh-server from starting on boot"
 update-rc.d -f ssh remove
+exit 0
--- hooks/02-nginx.sh
+++ hooks/02-nginx.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
 echo "I: installing nginx"
 apt-get install --yes --force-yes -y nginx
+echo "I: Enabling nginx to start on boot"
 update-rc.d -f nginx defaults
+exit 0
